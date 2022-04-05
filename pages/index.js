469ebc57009fb7c3ac849065a109235f3f86cb37
andy_metcalfe <80961807+andy-8980@users.noreply.github.com>
2022-04-05 19:55:16 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,9 @@ export default function Home({ posts }) {
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
+            <Categories /> 
             <PostWidget />
-            <Categories />
+            
           </div>
         </div>
       </div>
